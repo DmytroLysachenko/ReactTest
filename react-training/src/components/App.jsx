ea6e5e3b1a4bb622ec6858ae494bf60b8541bde0
Dimka863 <dlysachenko98@gmail.com>
2024-04-25 11:43:06 +0200

    innit commit

--- react-training/src/components/App.jsx
+++ react-training/src/components/App.jsx
@@ -1,20 +1,24 @@
-import { Product } from "./Product";
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" }
+];
 
-export default function App() {
+const BookList = ({ books }) => {
   return (
-    <div>
-      <h1>Best selling</h1>
+		<ul>
+			{books.map((book) => {
+ return <li key={book.id}>{book.name}</li>;
+			})}
+		</ul>
+  );s
+};
 
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-    </div>
+const App = () => {
+  return (
+    <>
+			<h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
+    </>
   );
-}
+};
